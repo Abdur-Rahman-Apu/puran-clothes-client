0d730ebc283d3a07e52cbd35b6a938042bfce673
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2022-11-24 21:59:08 +0600

    Navbar is created

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,9 +1,71 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const menuItems = <>
+        <li>
+            <Link to='/' className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</Link>
+        </li>
+        <li>
+            <Link to='/services' className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Categories</Link>
+        </li>
+        <li>
+            <Link to='/blog' className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</Link>
+        </li>
+
+
+    </>
     return (
-        <div>
-            <h1>This is navbar</h1>
+        <div className='w-100'>
+
+
+
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            {menuItems}
+                        </ul>
+                    </div>
+                    <Link to="/" className="flex items-center">
+                        <img src='./Logo.png' className="mr-3 md:h-10 sm:h-16" alt="Logo" />
+                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Puran Clothes</span>
+                    </Link>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul tabIndex={0} className=" menu menu-horizontal flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+
+                        {menuItems}
+
+                        {/* {
+                            user?.email ? <>
+                                <li>
+                                    <Link to='/addService' className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"> Add service</Link>
+                                </li>
+                                <li>
+                                    <Link to='/myReviews' className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">My Reviews</Link>
+                                </li>
+                            </> : ""
+                        } */}
+
+
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    {/* {
+                        user?.email ?
+                            <button onClick={handleLogOut} type="button" class="focus:outline-none  bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Log out</button>
+                            :
+                            <Link to='/login'><button type="button" class="focus:outline-none  bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:focus:ring-yellow-900">Log In</button></Link>
+                    } */}
+                </div>
+            </div>
+
+
+
         </div>
     );
 };
